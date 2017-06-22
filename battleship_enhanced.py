f8733e8a3f8fa62e813cd3f5822fe89ccc5af854
Gergely Papi <gergely.papi.cc@gmail.com>
2017-06-22 15:31:08 +0200

    Finalized features, added clear table feature after each action, as well as delayed messages on updates.

--- battleship_enhanced.py
+++ battleship_enhanced.py
@@ -1,6 +1,7 @@
 import string
 import random
 import sys
+import os
 import time
 
 ship_type_database = {4: "Carrier" , 5: "Battleship", 3: "Cruiser", 2: "Destroyer", 1: "Rambo"}
@@ -8,24 +9,28 @@ missmsg = ["Damn it, next time!", "Put on your glasses!", "Watch out!", "Missed.
 hitmsg = ["Nice shot!", "You rock!", "It hurts!", "Ouch!", "Surefire"]
 player1 = input("Please state your name: ")
 player2 = input("Please state your name: ")
+os.system("clear")
 shoot_count_tracker = [0, 0]
 sunken_ship_tracker = [[], []]
-num_row = list(range(1, 11))
-
-player1_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
+top_line = ["   " + str(i) + "   " for i in list(range(1, 11))]
 player1_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player2_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player1_shoot_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player2_shoot_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
-
 row_index_letter = list(string.ascii_lowercase[:10])
 
+def print_grid(current_grid):
+    print("  " + "".join(top_line))
+    for k, v in sorted(current_grid.items()):
+         print('{0}: {1}'.format(k,v))
+
 def ship_placement(player, ship_type_database, start_grid):
-    print("{0}'s turn".format(player))
     for key, value in sorted(ship_type_database.items(), reverse=True):
         while True:
             try:
-                ship_direction_input = input("Do you want to place your {0} horizontally or vertically? It occupies {1} coordinates. ".format(value, key))
+                print("{0}'s turn".format(player))
+                print_grid(start_grid)
+                ship_direction_input = input("Do you want to place your {0} horizontally or vertically? It occupies {1} coordinates.: ".format(value, key))
                 beginning_coordinate_input = input("Beginning coordinates of your {0}: ".format(value))
                 beginning_coordinate_key = beginning_coordinate_input[0]
                 beginning_coordinate_value = int(beginning_coordinate_input[1:]) - 1
@@ -49,10 +54,11 @@ def ship_placement(player, ship_type_database, start_grid):
                 break
             except:
                 print("It seems you made a mistake, please try your input again!")
+                time.sleep(2)
+                os.system("clear")
                 continue
 
-        for k, v in sorted(start_grid.items()):
-             print(k,v)
+        os.system("clear")
 
 ship_placement(player1, ship_type_database, player1_start_grid)
 ship_placement(player2, ship_type_database, player2_start_grid)
@@ -61,41 +67,54 @@ def player_turn(start_grid, shoot_grid, player, tracker):
     global shoot_count_tracker
     hit = True
     while hit == True:
-        print("{0}'s turn".format(player))
-        beginning_coordinate_input = input("Where do you want to shoot? ")
-        beginning_coordinate_key = beginning_coordinate_input[0]
-        beginning_coordinate_value = int(beginning_coordinate_input[1:]) -1
+        while True:
+            try:
+                print_grid(shoot_grid)
+                print("{0}'s turn".format(player))
+                beginning_coordinate_input = input("Where do you want to shoot? ")
+                beginning_coordinate_key = beginning_coordinate_input[0]
+                beginning_coordinate_value = int(beginning_coordinate_input[1:]) -1
 
-        if start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] != " X ":
-            shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " X "
-            print(hitmsg[random.randint(0, 4)])
-            ship_num = int(start_grid[beginning_coordinate_key][beginning_coordinate_value].strip())
-            sunken_ship_tracker[tracker].append(ship_num)
-            if ship_num == sunken_ship_tracker[tracker].count(ship_num):
-                print("You sunk {0}".format(ship_type_database[ship_num]))
-            for k, v in sorted(shoot_grid.items()):
-                 print(k,v)
-            hit = True
-            shoot_count_tracker[tracker] += 1
-            print(sunken_ship_tracker)
-            if shoot_count_tracker[0] == 15:
-                print("{0} winner pirate!".format(player1))
+                if start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] != " X ":
+                    shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " X "
+                    print(hitmsg[random.randint(0, 4)])
+
+                    ship_num = int(start_grid[beginning_coordinate_key][beginning_coordinate_value].strip())
+                    sunken_ship_tracker[tracker].append(ship_num)
+                    if ship_num == sunken_ship_tracker[tracker].count(ship_num):
+                        print("You sunk {0}".format(ship_type_database[ship_num]))
+                    hit = True
+                    shoot_count_tracker[tracker] += 1
+                    if shoot_count_tracker[0] == 15:
+                        print("{0} winner pirate!".format(player1))
+                        raise NameError
+                    elif shoot_count_tracker[1] == 15:
+                        print("{0} winner pirate!".format(player2))
+                        raise NameError
+                    time.sleep(2)
+                    os.system("clear")
+                    continue
+                elif start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] == " X ":
+                    print ("You fucking bastard, you prick, you have already tried to shoot here!")
+                    hit = False
+                    time.sleep(2)
+                    os.system("clear")
+                    break
+                else:
+                    print(missmsg[random.randint(0, 4)])
+                    shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " I "
+                    hit = False
+                    time.sleep(2)
+                    os.system("clear")
+                    break
+                break
+            except NameError:
                 sys.exit()
-            elif shoot_count_tracker[1] == 15:
-                print("{0} winner pirate!".format(player2))
-                sys.exit()
-            continue
-        elif start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] == " X ":
-            print ("You fucking bastard, you prick, you have already tried to shoot here!")
-            hit = False
-            break
-        else:
-            print(missmsg[random.randint(0, 4)])
-            shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " I "
-            for k, v in sorted(shoot_grid.items()):
-                 print(k,v)
-            hit = False
-            break
+            except:
+                print("You fuck, you misstyped the coordinate")
+                time.sleep(2)
+                os.system("clear")
+                continue
 
 while True:
     player_turn(player2_start_grid, player1_shoot_grid, player1, 0)
