a7ae6728518ff5ba9bb72564b30609468818e857
Gergely Papi <gergely.papi.cc@gmail.com>
2017-06-22 13:18:12 +0200

    Ship overlap problem solved, player can not shoot to the same coordinate twice

--- battleship_enhanced.py
+++ battleship_enhanced.py
@@ -1,15 +1,18 @@
 import string
 import random
 import sys
+import time
 
 ship_type_database = {4: "Carrier" , 5: "Battleship", 3: "Cruiser", 2: "Destroyer", 1: "Rambo"}
-missmsg = ["Damn it, next time!", "Put on your glasses!", "Watch out!", "Missed..."]
-hitmsg = ["Nice shot!", "You rock!", "It hurts!", "Ouch!"]
+missmsg = ["Damn it, next time!", "Put on your glasses!", "Watch out!", "Missed...", "Buzadara"]
+hitmsg = ["Nice shot!", "You rock!", "It hurts!", "Ouch!", "Surefire"]
 player1 = input("Please state your name: ")
 player2 = input("Please state your name: ")
 shoot_count_tracker = [0, 0]
 sunken_ship_tracker = [[], []]
+num_row = list(range(1, 11))
 
+player1_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player1_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player2_start_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 player1_shoot_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
@@ -17,56 +20,62 @@ player2_shoot_grid = {key:[" 0 "] * 10 for key in string.ascii_lowercase[:10]}
 
 row_index_letter = list(string.ascii_lowercase[:10])
 
-def ship_placement(ship_type_database, start_grid):
-    while True:
-        try:
-            for key, value in sorted(ship_type_database.items(), reverse=True):
+def ship_placement(player, ship_type_database, start_grid):
+    print("{0}'s turn".format(player))
+    for key, value in sorted(ship_type_database.items(), reverse=True):
+        while True:
+            try:
                 ship_direction_input = input("Do you want to place your {0} horizontally or vertically? It occupies {1} coordinates. ".format(value, key))
                 beginning_coordinate_input = input("Beginning coordinates of your {0}: ".format(value))
                 beginning_coordinate_key = beginning_coordinate_input[0]
                 beginning_coordinate_value = int(beginning_coordinate_input[1:]) - 1
 
                 if ship_direction_input[0] == "h":
+                    check_ship = []
                     for i in range(key):
-                        if start_grid[beginning_coordinate_key][i+beginning_coordinate_value] == " 0 ":
+                        check_ship.append(start_grid[beginning_coordinate_key][i+beginning_coordinate_value])
+                        if not " 0 " in check_ship:
+                            raise ValueError
+                        else:
                             start_grid[beginning_coordinate_key][i+beginning_coordinate_value] = " " + str(key) + " "
-                        else:
-                            raise ValueError
-                    for k, v in sorted(start_grid.items()):
-                         print(k, v)
                 elif ship_direction_input[0] == "v":
+                    check_ship = []
                     for letter in row_index_letter[row_index_letter.index(beginning_coordinate_key):row_index_letter.index(beginning_coordinate_key)+key]:
-                        if start_grid[letter][beginning_coordinate_value] == " 0 ":
-                            start_grid[letter][beginning_coordinate_value] = " " + str(key) + " "
-                        else:
+                        check_ship.append(start_grid[letter][beginning_coordinate_value])
+                        if not " 0 " in check_ship:
                             raise ValueError
-                    for k, v in sorted(start_grid.items()):
-                         print(k,v)
-            break
-        except:
-            print("Oops, it seems you made a mistake. Please try again.")
-            continue
+                        else:
+                            start_grid[letter][beginning_coordinate_value] = " " + str(key) + " "
+                break
+            except:
+                print("It seems you made a mistake, please try your input again!")
+                continue
 
-ship_placement(ship_type_database, player1_start_grid)
-ship_placement(ship_type_database, player2_start_grid)
+        for k, v in sorted(start_grid.items()):
+             print(k,v)
+
+ship_placement(player1, ship_type_database, player1_start_grid)
+ship_placement(player2, ship_type_database, player2_start_grid)
 
 def player_turn(start_grid, shoot_grid, player, tracker):
     global shoot_count_tracker
-    while True:
+    hit = True
+    while hit == True:
         print("{0}'s turn".format(player))
         beginning_coordinate_input = input("Where do you want to shoot? ")
         beginning_coordinate_key = beginning_coordinate_input[0]
         beginning_coordinate_value = int(beginning_coordinate_input[1:]) -1
 
-        if start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 ":
+        if start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] != " X ":
             shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " X "
-            print(hitmsg[random.randint(0, 3)])
+            print(hitmsg[random.randint(0, 4)])
             ship_num = int(start_grid[beginning_coordinate_key][beginning_coordinate_value].strip())
             sunken_ship_tracker[tracker].append(ship_num)
             if ship_num == sunken_ship_tracker[tracker].count(ship_num):
                 print("You sunk {0}".format(ship_type_database[ship_num]))
             for k, v in sorted(shoot_grid.items()):
                  print(k,v)
+            hit = True
             shoot_count_tracker[tracker] += 1
             print(sunken_ship_tracker)
             if shoot_count_tracker[0] == 15:
@@ -76,11 +85,16 @@ def player_turn(start_grid, shoot_grid, player, tracker):
                 print("{0} winner pirate!".format(player2))
                 sys.exit()
             continue
+        elif start_grid[beginning_coordinate_key][beginning_coordinate_value] != " 0 " and shoot_grid[beginning_coordinate_key][beginning_coordinate_value] == " X ":
+            print ("You fucking bastard, you prick, you have already tried to shoot here!")
+            hit = False
+            break
         else:
-            print(missmsg[random.randint(0, 3)])
+            print(missmsg[random.randint(0, 4)])
             shoot_grid[beginning_coordinate_key][beginning_coordinate_value] = " I "
             for k, v in sorted(shoot_grid.items()):
                  print(k,v)
+            hit = False
             break
 
 while True:
